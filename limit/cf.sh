3ff5d737de16299ebe41cdde907d14070652cb41
yBhIZaSVDZbWseNP <budi.stmikdw@gmail.com>
2024-08-15 05:23:05 +0700

    V4 - NO IZIN IP

---
+++ limit/cf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+MYIP=$(wget -qO- icanhazip.com)
+apt install jq curl -y
+
+# Domain utama yang ditetapkan
+DOMAIN=klmpk.systems
+
+# Membuat subdomain secara acak dengan domain utama
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+dns=${sub}.$DOMAIN
+
+# Kredensial Cloudflare
+CF_ID=andyyuda41@gmail.com
+CF_KEY=44ca6e372a82e9ca0d4b1f269302ebabea0f2
+
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com)
+
+echo "Updating DNS for ${dns}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+
+echo "$dns" > /root/domain
+echo "$dns" > /root/scdomain
+echo "$dns" > /etc/xray/domain
+echo "$dns" > /etc/v2ray/domain
+echo "$dns" > /etc/xray/scdomain
+echo "IP=$dns" > /var/lib/klmpk/ipvps.conf
+cd
